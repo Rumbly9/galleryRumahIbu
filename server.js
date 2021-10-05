b5e3305a46e2a7de0146376b6e0774c0d8e59b55
Aulia <aulachmadgmail.com>
2021-10-05 15:08:44 +0700

    fixed static files codes

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express()
 
 const PORT = 8000
 
+app.use(express.static("/"))
+app.use("/css", express.static(__dirname + "/css"))
+app.use("/assets", express.static(__dirname + "/assets"))
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
-  res.sendFile(__dirname + "/css/normalize.css")
-  res.sendFile(__dirname + "/css/style.css")
 })
 
 app.listen(process.env.PORT || PORT, () => {
